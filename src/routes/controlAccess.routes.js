255c026b0f40d7baeabfe7df5c62e61851dea9f9
jose chirinos <josechirinos11@gmail.com>
2025-05-27 11:24:51 +0200

    consulta access con doble backend  1.5

--- src/routes/controlAccess.routes.js
+++ src/routes/controlAccess.routes.js
@@ -1,19 +1,20 @@
 // src/routes/controlAccess.routes.js
 require('dotenv').config();
 const express = require('express');
-const fetch   = require('node-fetch');
+
 const router  = express.Router();
 
 const PROXY = process.env.PROXY_URL;
 
+
+
 router.get('/formularioControlPedido', async (req, res) => {
   try {
-    const response = await fetch(PROXY);
-    if (!response.ok) throw new Error(response.statusText);
+    const response = await fetch('http://192.168.1.81:3001/api/pedidos');
     const data = await response.json();
     res.json(data);
   } catch (err) {
-    console.error('Error consumiendo el proxy:', err);
+   console.error('Error consumiendo el proxy:', err);
     res.status(500).json({ error: err.message });
   }
 });
