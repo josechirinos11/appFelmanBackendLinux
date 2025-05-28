7ce06b0699ad612b35b9bd8651cda3bb9641a849
josechirinos11 <josechirinos11@gmail.com>
2025-05-28 18:35:28 +0200

    ruta incidenci 1.0

--- src/routes/controlAccess.routes.js
+++ src/routes/controlAccess.routes.js
@@ -8,7 +8,6 @@ const router  = express.Router();
 const PROXY = process.env.PROXY_URL;
 
 
-
 router.get('/formularioControlPedido', async (req, res) => {
   try {
     const response = await fetch('http://192.168.1.81:3001/api/pedidos');
@@ -23,6 +22,18 @@ router.get('/formularioControlPedido', async (req, res) => {
 router.get('/ConsultaControlPedidoInicio', async (req, res) => {
   try {
     const response = await fetch('http://192.168.1.81:3001/api/controlPedidoInicio');
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+   console.error('Error consumiendo el proxy:', err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+router.get('/incidencia', async (req, res) => {
+  try {
+    const response = await fetch('http://192.168.1.81:3001/api/incidencia');
     const data = await response.json();
     res.json(data);
   } catch (err) {
