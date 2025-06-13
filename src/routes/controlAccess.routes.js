80812affb00e974cb9ebf23ff5fc3f575d985a3f
josechirinos11 <josechirinos11@gmail.com>
2025-06-13 19:15:21 +0200

    carga controlPedidoInicio40Registro

--- src/routes/controlAccess.routes.js
+++ src/routes/controlAccess.routes.js
@@ -31,6 +31,17 @@ router.get('/ConsultaControlPedidoInicio', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+router.get('/ConsultaControlPedidoInicio40Registro', async (req, res) => {
+  try {
+    const response = await fetch('http://192.168.1.81:3001/api/controlPedidoInicio40Registro');
+    const data = await response.json();
+    console.log('Datos recibidos ACCESS ConsultaControlPedidoInicio');
+    res.json(data);
+  } catch (err) {
+   console.error('Error consumiendo el proxy:', err);
+    res.status(500).json({ error: err.message });
+  }
+});
 
 
 router.get('/controlEntregaDiaria', async (req, res) => {
