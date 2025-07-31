da43d8bb8bb26989b014cc0050b9486a8afe28bb
jose chirinos <josechirinos11@gmail.com>
2025-07-31 13:44:05 +0200

    cargando ruta almacen al backend 1.1

--- src/routes/controlAlmacen.router.js
+++ src/routes/controlAlmacen.router.js
@@ -4,18 +4,18 @@ const pool = require('../config/databaseAlamcen');
 const router = express.Router();
 
 router.get('/inicio', async (req, res) => {
-  try {
-    const [result] = await pool.execute('SHOW TABLES');
-    res.status(200).json(result);
-  } catch (error) {
-    console.error('❌ ERROR EN /control-almacen/inicio:', error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Error interno del servidor',
-      detail: error.message, // Esto te mostrará el error exacto
-    });
-  }
-});
+    try {
+      const [result] = await pool.execute('SELECT NOW() AS fecha_servidor');
+      res.status(200).json(result);
+    } catch (error) {
+      console.error('❌ ERROR EN /control-almacen/inicio:', error);
+      res.status(500).json({
+        status: 'error',
+        message: 'Error interno del servidor',
+        detail: error.message,
+      });
+    }
+  });
 
 
 
